3ec0b4a12852f7a6d0b8d4a56cc0eef171f37627
jonathan <jonathan@kyuss.org>
2014-07-16 12:04:38 -0700

    changes to support cloudstead packaging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,57 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    unless params[:email]
+      logger.info("no email param: #{params}")
+      redirect_to login_url
+      return
+    end
 
-    if UserAuth.new(user).login(params[:password])
-      session[:session_key] = user.session_key
+    # auth user against cloudOs.
+    account_name = params[:email].partition('@').first # just the username, no @whateverdomain
+    options = {
+        :headers => { 'Content-Type' => 'application/json' },
+        :body => {
+            :name => account_name,
+            :password => params[:password]
+        }.to_json
+    }
+    response = HTTParty.post('http://127.0.0.1:3001/api/accounts', options)
+
+    if response.code == 200
+      # ensure user exists in our DB
+      db_user = User.find_by_email(params[:email])
+      unless db_user
+        db_user = User.new
+        db_user.username = account_name
+        db_user.email = params[:email]
+        db_user.password = SecureRandom.urlsafe_base64(32) # password does not matter
+        db_user.save
+
+        Board.all.each do |board|
+          # add user to all boards
+          board_member = BoardMember.new
+          board_member.member = db_user
+          board_member.board = board
+          board_member.admin = true
+          board_member.save
+        end
+      end
+
+      session[:session_key] = SecureRandom.urlsafe_base64(32)
+      db_user.update_attribute(:session_key, session[:session_key])
       redirect_to root_url
+
     else
       # TODO: add errors
+      logger.info("auth bombed: #{response.code}")
+
       redirect_to login_url
     end
   end
 
   def destroy
-    UserAuth.new(current_user).logout
+    current_user.update_attribute(:session_key, nil)
     reset_session
     redirect_to login_url
   end
