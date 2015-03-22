a67db23240206f452c6c8628f94c89b2270f65f9
jonathan <jonathan@kyuss.org>
2015-03-22 10:43:33 -0700

    serve static assets from rails for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Kanban::Application.configure do
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
